ddce60e948291040f50875281c62a11b2de5bcd5
gamedevCloudy <acaayush1111@gmail.com>
2022-09-23 09:16:00 +0530

    Init.
    full

---
+++ Array Sum/matrixAddn.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std; 
+
+void AddMatrix(int m1[], int m2[])
+{
+    cout<<"hello"; 
+}
+
+int m1[2][2] = {1,2,3,4}; 
+
+for(int i =0; i<2; i++)
+{
+    for(int j =0; j<2; i++)
+    {
+        cout<<m1[i]<<m1[j]<<endl; 
+    }
+
+}
