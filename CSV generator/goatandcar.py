3a9229869e59b59022024e2da9d00bf32c6f229c
gamedevCloudy <acaayush1111@gmail.com>
2022-10-14 23:53:14 +0530

    Update goatandcar.py

--- CSV generator/goatandcar.py
+++ CSV generator/goatandcar.py
@@ -2,6 +2,8 @@
 import random as r
 from re import X;
 arre =[]
+sarr = []
+
 
 def GenArrays():
     for i in range(100):
@@ -18,13 +20,12 @@ def GenArrays():
                 x[2] = 1
         arre.append(x)
 
-sarr = []
-
 def GetSample(): 
     for i in arre:
         if(i[2]==0): 
             # print("hello", i)
             sarr.append(i)
+
 
 def ChooseDoor():
     x,y =0,0
@@ -32,6 +33,7 @@ def ChooseDoor():
         if i[0]==1: x+=1
         if i[1]==1: y+=1
     return x,y
+
 
 def ChangeOpinion():
     counter= 0
@@ -43,23 +45,25 @@ def ChangeOpinion():
     return (probwin)
 
 
-won = []
-sampleCount = 0
-couldWinIfDintChange = 0
-couldWinIfDidChange = 0
-for i in range(100): 
-    GenArrays()
-    GetSample()
-    sampleCount += (len(sarr))
-    couldWinIfDintChange,couldWinIfDidChange =   ChooseDoor()
-    won.append(ChangeOpinion())
+def main(): 
+    won = []
+    sampleCount = 0
+    couldWinIfDintChange = 0
+    couldWinIfDidChange = 0
+    for i in range(100): 
+        GenArrays()
+        GetSample()
+        sampleCount += (len(sarr))
+        couldWinIfDintChange,couldWinIfDidChange =   ChooseDoor()
+        won.append(ChangeOpinion())
 
-ct = 0
-for i in won: 
-    ct+=i
-averangeChanceofWin = ct/len(won)
-percentageChange = round(averangeChanceofWin,5)*100
+    ct = 0
+    for i in won: 
+        ct+=i
+    averangeChanceofWin = ct/len(won)
+    percentageChange = round(averangeChanceofWin,5)*100
 
-print("Samples:", sampleCount, "\t Door1: ", couldWinIfDintChange, "\t Door2: ", couldWinIfDidChange, "\n Average percentage chance of winning if door was changed: ", percentageChange)
-            
-            
+    print("Samples:", sampleCount, "\t Door1: ", couldWinIfDintChange, "\t Door2: ", couldWinIfDidChange, "\n Average percentage chance of winning if door was changed: ", percentageChange)
+
+
+main()           
